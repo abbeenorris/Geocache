f24a8e2ae80090e50199ff89a765eb4abb14eede
Abigail Norris <abbeenorris@hotmail.co.uk>
2016-01-12 12:24:18 +0000

    collection view controller
    Still can't work it

--- test/PostcardViewController.swift
+++ test/PostcardViewController.swift
@@ -2,7 +2,6 @@ import UIKit
 
 class PostcardViewController: UIViewController {
     
-    @IBOutlet weak var myImage: UICollectionViewCell!
     override func viewDidLoad() {
         super.viewDidLoad()
         
