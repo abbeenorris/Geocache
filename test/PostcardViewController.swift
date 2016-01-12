ff3c79aa1235ffbda0f86e8b4bf8b4876d96928e
Abigail Norris <abbeenorris@hotmail.co.uk>
2016-01-12 12:06:45 +0000

    trying to insert a gird of 9 for images
    not working

--- test/PostcardViewController.swift
+++ test/PostcardViewController.swift
@@ -2,10 +2,11 @@ import UIKit
 
 class PostcardViewController: UIViewController {
     
+    @IBOutlet weak var myImage: UICollectionViewCell!
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
-    
+
 }
