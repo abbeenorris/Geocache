e1f8424b0ed97e1d0b1f48745d9fbed8570f10e1
Abigail Norris <abbeenorris@hotmail.co.uk>
2016-01-12 21:35:45 +0000

    Creating image randomiser
    I created a randomiser to allow users to push a button to view random
    images.

--- test/PostcardViewController.swift
+++ test/PostcardViewController.swift
@@ -2,11 +2,29 @@ import UIKit
 
 class PostcardViewController: UIViewController {
     
+    @IBOutlet weak var firstImageView: UIImageView!
+    @IBOutlet weak var playRoundButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.playRoundButton.setTitle("Next", forState: UIControlState.Normal)
+        
     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func playRoundTapped(sender: UIButton) {
+        
+        var firstRandomNumber = arc4random_uniform(6) + 1
+        var firstCardString:String = String(format: "Geo%i", firstRandomNumber)
+    
+        self.firstImageView.image = UIImage(named: firstCardString)
+
+    }
+    
     
 }
 
