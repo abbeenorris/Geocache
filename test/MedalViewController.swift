7766568bb62bc3cc53ee8d7d20a4eda41659ad7f
Abigail Norris <abbeenorris@hotmail.co.uk>
2016-01-12 10:38:23 +0000

    Added two more files, PoppyViewController and DogtagViewController
    This is so i can create new view controllers and put content in them
    for the geocache

---
+++ test/MedalViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class MedalViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
---
+++ test/PoppyViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class PoppyViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
